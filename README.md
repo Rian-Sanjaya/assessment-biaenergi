35bd339e858ef21e8a3b8d9f30bbe446ef688b5f
Rian Sanjaya <rian.sj@gmail.com>
2023-06-12 14:38:16 +0700

    create readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# assessment-biaenergi
+
+Assessment Test BIAENERGI bagian Test Logika dan Algoritma
+
+## Jawaban Soal:
+
+**No. 1** di file **logika_algoritma_1.js**
+
+**No. 2a** di file **logika_algoritma_2a.js**
+
+**No. 2b** di file **logika_algoritma_2b.js**
+
+**No. 2c** di file **logika_algoritma_2c.js**
+
+**No. 2d** di file **logika_algoritma_2d.js**
+
+## Untuk menjalankan:
+
+(install node.js terlebih dahulu bila belum ada)
+
+```bash
+$ node logika_algoritma_1.js 
+```
+
+```bash
+$ node logika_algoritma_2a.js 
+```
+
+```bash
+$ node logika_algoritma_2b.js 
+```
+
+```bash
+$ node logika_algoritma_2c.js 
+```
+
+```bash
+$ node logika_algoritma_2d.js 
+```
--- logika_algoritma_2b.js
+++ logika_algoritma_2b.js
@@ -1,14 +1,33 @@
 function TambahBilanganGanjil (len) {
-  const result = [1, 2];
+  const result = [];
+
+  if (len > 0) result[0] = 1;
+
+  if (len > 1) result[1] = 2;
+
   let count = 2;
   let nextOdd = 1
+
   while (count < len) {
     nextOdd += 2;
     result[count] = result[count-1] + nextOdd;
     count++;
   }
+
   return result;
 }
 
-const len = 11;
-console.log(TambahBilanganGanjil(len));
+let len = 0;
+console.log('input 0: ', TambahBilanganGanjil(len));
+
+len = 1;
+console.log('input 1: ', TambahBilanganGanjil(len));
+
+len = 2;
+console.log('input 2: ', TambahBilanganGanjil(len));
+
+len = 3;
+console.log('input 3: ', TambahBilanganGanjil(len));
+
+len = 11;
+console.log('input 11: ', TambahBilanganGanjil(len));
